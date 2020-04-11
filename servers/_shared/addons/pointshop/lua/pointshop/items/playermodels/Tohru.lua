2b437c6c6355ca071b02dc9af9eccd2e6da862e1
Natanfc100 <32806615+Natanfc100@users.noreply.github.com>
2020-04-11 13:50:46 -0300

    Arrumei Tohru

--- servers/_shared/addons/pointshop/lua/pointshop/items/playermodels/Tohru.lua
+++ servers/_shared/addons/pointshop/lua/pointshop/items/playermodels/Tohru.lua
@@ -1,3 +1,3 @@
 ITEM.Name = "Tohru"
 ITEM.Price = 20000
-ITEM.Model = "Tohru", "models/player/dewobedil/maid_dragon/tohru/default_p.mdl"
+ITEM.Model ="models/player/dewobedil/maid_dragon/tohru/default_p.mdl"
